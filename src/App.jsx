66fafe804732a7a175c2aaec0807dbeb80387c8a
Matt Dobson <mattdobson@Matts-MacBook-Pro.local>
2025-05-22 17:09:44 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Hello, Louisville 👋</h1>
+      <p>This is the start of <strong>What Could Lou Do</strong>.</p>
+      <p>We're building a place to imagine the future of our shared spaces.</p>
+    </div>
+  );
+}
+
+
+
+export default App
